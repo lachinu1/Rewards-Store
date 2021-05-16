0af62b711adf948733344a02002ee2e140b80570
Chinu <lachinu1@gmail.com>
2021-05-15 22:55:40 -0300

    Maquetado header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
-
+import React from 'react';
+import Header from './Components/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Holaaaaaaaaaa</h1>
+      <Header />
     </div>
   );
 }
